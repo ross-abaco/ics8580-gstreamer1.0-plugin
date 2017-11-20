61429e108b3668cbc4dee15a682534cf09a201d4
Ross Newman <ross.newman@abaco.com>
2017-11-20 15:15:06 +0000

    Added webcam pipeline.

--- gst-template-sink/gst-plugin/test01-tv.sh
+++ gst-template-sink/gst-plugin/test01-tv.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export GST_DEBUG=3,gst_8580sink_debug:*
-#gst-launch-1.0 -v videotestsrc pattern=0 horizontal-speed=0 ! "video/x-raw, width=720, height=576" ! 8580sink output=2 type=1 res=2 channel=1 sync=true
-gst-launch-1.0 -v videotestsrc pattern=0 horizontal-speed=0 ! "video/x-raw, width=720, height=576" ! 8580sink output=2 type=1 res=2 channel=1 sync=true
-#gst-launch-1.0 -v videotestsrc pattern=11 ! "video/x-raw, width=720, height=576" ! 8580sink output=2 type=1 res=2 channel=1 sync=true
+gst-launch-1.0 -v v4l2src ! "video/x-raw, width=640, height=480" ! videoscale ! videoconvert ! "video/x-raw, width=720, height=576" ! 8580sink output=2 type=1 res=2 channel=1 sync=false
+#gst-launch-1.0 -v videotestsrc horizontal-speed=1 ! "video/x-raw, width=720, height=576" ! 8580sink output=2 type=1 res=2 channel=1 sync=true
 
